9d82147c12bf435b888bbed4d6073ed7a2aaf0dc
exp_table <exp.table@protonmail.com>
2024-04-18 16:33:50 +0200

    commented out endBlock

--- ponder.config.ts
+++ ponder.config.ts
@@ -26,17 +26,17 @@ export default createConfig({
         ethereum: {
           address: "0xB39DF6BBB1Cf2B609DeE43F109caFEFF1A7CCBEa",
           startBlock: 19672108,
-          endBlock: 19716773,
+          //endBlock: 19716773,
         },
         arbitrum: {
           address: "0xCa34d7cc253b47E0248b80c859F38a658db7BcCC",
           startBlock: 201662549,
-          endBlock: 203684249,
+          //endBlock: 203684249,
         },
         base: {
           address: "0xB39DF6BBB1Cf2B609DeE43F109caFEFF1A7CCBEa",
           startBlock: 13264923,
-          endBlock: 13534940,
+          //endBlock: 13534940,
         },
       },
     },
