0613493ff0cdee9d4f8e6d10ce793efe73e4ebd5
exp_table <exp.table@protonmail.com>
2024-04-18 19:06:21 +0200

    Hook: changed type of id

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -2,7 +2,7 @@ import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
   HookDeposit: p.createTable({
-    id: p.hex(), // address of user
+    id: p.string(), // address of user
     deposited: p.bigint(),
   }),
 }));
--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,8 @@ ponder.on("HookVault:TokensDeposited", async ({ event, context }) => {
   if (event.block.timestamp < 1713414000 || event.block.timestamp > 1713846000)
     return;
   const { HookDeposit } = context.db;
-  const user = await HookDeposit.findUnique({
-    id: event.transaction.from,
-  });
   const token = await HookDeposit.upsert({
-    id: event.transaction.from,
+    id: event.transaction.from.toString(),
     create: {
       deposited: event.args.depositAmount,
     },
