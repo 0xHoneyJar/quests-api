0d4342dbe5de051baf31820bf26c5cc9c7d33f98
exp_table <exp.table@protonmail.com>
2024-04-28 16:11:14 +0200

    removed Hook

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,8 +1,3 @@
 import { createSchema } from "@ponder/core";
 
-export default createSchema((p) => ({
-  HookDeposit: p.createTable({
-    id: p.string(), // address of user
-    deposited: p.bigint(),
-  }),
-}));
+export default createSchema((p) => ({}));
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,1 @@
 import { ponder } from "@/generated";
-
-ponder.on("HookVault:TokensDeposited", async ({ event, context }) => {
-  if (event.block.timestamp < 1713414000 || event.block.timestamp > 1713846000)
-    return;
-  const { HookDeposit } = context.db;
-  const token = await HookDeposit.upsert({
-    id: event.transaction.from.toString(),
-    create: {
-      deposited: event.args.depositAmount,
-    },
-    update: ({ current }) => ({
-      deposited: current.deposited + event.args.depositAmount,
-    }),
-  });
-});
