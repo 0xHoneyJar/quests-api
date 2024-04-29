3d14b574c7be4e08bfd545020ef2c75acb0e1b36
exp_table <exp.table@protonmail.com>
2024-04-29 11:13:35 +0200

    reintroduced Hook for db debugging

--- ponder.config.ts
+++ ponder.config.ts
@@ -2,6 +2,7 @@ import { createConfig } from "@ponder/core";
 import { http } from "viem";
 
 import { erc1155Abi } from "./abis/erc1155";
+import { hookVaultAbi } from "./abis/hookVault";
 
 export default createConfig({
   database: {
@@ -36,6 +37,27 @@ export default createConfig({
         base: {
           address: "0x6cfb9280767a3596ee6af887d900014a755ffc75",
           startBlock: 13791613, // TBD
+        },
+      },
+    },
+    HookVault: {
+      abi: hookVaultAbi,
+      filter: { event: "TokensDeposited" },
+      network: {
+        ethereum: {
+          address: "0xB39DF6BBB1Cf2B609DeE43F109caFEFF1A7CCBEa",
+          startBlock: 19672108,
+          endBlock: 19716773,
+        },
+        arbitrum: {
+          address: "0xCa34d7cc253b47E0248b80c859F38a658db7BcCC",
+          startBlock: 201662549,
+          endBlock: 203684249,
+        },
+        base: {
+          address: "0xB39DF6BBB1Cf2B609DeE43F109caFEFF1A7CCBEa",
+          startBlock: 13264923,
+          endBlock: 13534940,
         },
       },
     },
