e495585f4650be43a846876a70efa617bc0702d2
soju <underrated@gmail.com>
2024-05-20 20:54:15 -0700

    Update ponder.schema.ts

--- ponder.schema.ts
+++ ponder.schema.ts
@@ -1,14 +1,14 @@
 import { createSchema } from "@ponder/core";
 
 export default createSchema((p) => ({
-  ApicultureMint: p.createTable({
-    id: p.string(),
-    quantity: p.bigint(),
-  }),
-  HookDeposit: p.createTable({
-    id: p.string(), // address of user
-    deposited: p.bigint(),
-  }),
+  // ApicultureMint: p.createTable({
+  //   id: p.string(),
+  //   quantity: p.bigint(),
+  // }),
+  // HookDeposit: p.createTable({
+  //   id: p.string(), // address of user
+  //   deposited: p.bigint(),
+  // }),
   THJ101Guide: p.createTable({
     id: p.string(),
     minted: p.boolean(),
